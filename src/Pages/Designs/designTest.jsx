ab87bd452411e0bf7f6a6dc28083bd704972cead
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-06-23 21:47:34 -0400

    added job scraper to portfolio and testing formatting on design page

--- src/Pages/Designs/designTest.jsx
+++ src/Pages/Designs/designTest.jsx
@@ -4,20 +4,32 @@ import Item from "@mui/material/Grid"
 export default function testDesign() {
     return (
         <>
-            <Grid container spacing={1}>
-                <Grid container spacing={1} item xs={6}>
-                    <Item xs={6} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                        <img style={{ height: '100px' }} src="/Helldiver-Tyler.png" alt="" />
-                    </Item>
-                    <Item xs={6} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>Space 2
-                        <img style={{ height: '100px' }} src="/Helldiver-Tyler.png" alt="" />
-                    </Item>
+            <Grid container spacing={2}>
+                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ height: '100px' }} src="../../../public/Helldiver-Tyler.png" alt="" />
+                        </Item>
+                    </Grid>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ height: '100px' }} src="../../../public/Helldiver-Tyler.png" alt="" />
+                        </Item>
+                    </Grid>
                 </Grid>
-                <Grid container spacing={1} item xs={6}>
-                    <Item xs={6} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}> Space 3</Item>
-                    <Item xs={6} style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}> Space 4</Item>
+                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ height: '100px' }} src="../../../public/Helldiver-Tyler.png" alt="" />
+                        </Item>
+                    </Grid>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ height: '100px' }} src="../../../public/Helldiver-Tyler.png" alt="" />
+                        </Item>
+                    </Grid>
                 </Grid>
-            </Grid>
+            </Grid >
         </>
     )
 }
