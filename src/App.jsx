968ba0ce674ff28f4036baffb9d27dc3eab591b1
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-07-03 12:51:57 -0400

    updated background image import under app.jsx and updated svg settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Outlet } from "react-router-dom";
+import './assets/Test-Portfolio-BackGround.svg'
 import Footer from "./Pages/Home/Footer/Footer";
 import HeaderSection from "./Pages/Home/Header/HeaderToApp";
 
