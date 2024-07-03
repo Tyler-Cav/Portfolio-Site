cfbece4d5915ff3f4d1de6ce31f57451a7344e26
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-07-03 15:09:51 -0400

    changed design page images to correctly scale with maxwidth and auto heigh

--- src/Pages/Designs/designTest.jsx
+++ src/Pages/Designs/designTest.jsx
@@ -11,13 +11,13 @@ export default function testDesign() {
             <Grid container spacing={2}>
                 <Grid container spacing={2} item lg={6} sm={12} xs={12}>
                     <Grid item lg={6} sm={12} xs={12}>
-                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ height: '100px' }} src={HellDivers} alt="" />
+                        <Item sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxWidth: '90%', height: 'auto' }} src={HellDivers} alt="" />
                         </Item>
                     </Grid>
                     <Grid item lg={6} sm={12} xs={12}>
                         <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ height: '60px' }} src={Glitch} alt="" />
+                            <img style={{ maxWidth: '90%', height: 'auto' }} src={Glitch} alt="" />
                         </Item>
                     </Grid>
                 </Grid>
@@ -29,7 +29,7 @@ export default function testDesign() {
                     </Grid>
                     <Grid item lg={6} sm={12} xs={12}>
                         <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ height: '200px', margin: '1px' }} src={CuteSkull} alt="" />
+                            <img style={{ maxWidth: '90%', height: 'auto', margin: '1px' }} src={CuteSkull} alt="" />
                         </Item>
                     </Grid>
                 </Grid>
