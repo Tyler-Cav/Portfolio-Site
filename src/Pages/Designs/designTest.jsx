d76a6597a543e164da65cf4bad67eee60d061b24
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-07-08 22:30:45 -0400

    removed a fragment that wasn't needed

--- src/Pages/Designs/designTest.jsx
+++ src/Pages/Designs/designTest.jsx
@@ -7,34 +7,32 @@ import CuteSkull from '../../assets/CuteSkull.png'
 
 export default function testDesign() {
     return (
-        <>
-            <Grid container spacing={2}>
-                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
-                    <Grid item lg={6} sm={12} xs={12}>
-                        <Item sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ maxWidth: '90%', height: 'auto' }} src={HellDivers} alt="" />
-                        </Item>
-                    </Grid>
-                    <Grid item lg={6} sm={12} xs={12}>
-                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ maxWidth: '90%', height: 'auto' }} src={Glitch} alt="" />
-                        </Item>
-                    </Grid>
+        <Grid container spacing={2}>
+            <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                <Grid item lg={6} sm={12} xs={12}>
+                    <Item sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                        <img style={{ maxWidth: '90%', height: 'auto' }} src={HellDivers} alt="" />
+                    </Item>
                 </Grid>
-                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
-                    <Grid item lg={6} sm={12} xs={12}>
-                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ height: '400px', maxWidth: '400px', borderRadius: '2%' }} src={TestBackground} alt="" />
-                        </Item>
-                    </Grid>
-                    <Grid item lg={6} sm={12} xs={12}>
-                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                            <img style={{ maxWidth: '90%', height: 'auto', margin: '1px' }} src={CuteSkull} alt="" />
-                        </Item>
-                    </Grid>
+                <Grid item lg={6} sm={12} xs={12}>
+                    <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                        <img style={{ maxWidth: '90%', height: 'auto' }} src={Glitch} alt="" />
+                    </Item>
                 </Grid>
-            </Grid >
-        </>
+            </Grid>
+            <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                <Grid item lg={6} sm={12} xs={12}>
+                    <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                        <img style={{ height: '400px', maxWidth: '400px', borderRadius: '2%' }} src={TestBackground} alt="" />
+                    </Item>
+                </Grid>
+                <Grid item lg={6} sm={12} xs={12}>
+                    <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                        <img style={{ maxWidth: '90%', height: 'auto', margin: '1px' }} src={CuteSkull} alt="" />
+                    </Item>
+                </Grid>
+            </Grid>
+        </Grid >
     )
 }
 
