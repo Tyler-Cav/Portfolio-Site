bded4c9d1ca435869fed229cbd13b9f21c9d561e
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-07-22 17:35:22 -0400

    added more designs

--- src/Pages/Designs/designTest.jsx
+++ src/Pages/Designs/designTest.jsx
@@ -4,35 +4,67 @@ import HellDivers from '../../assets/Helldiver-Tyler.png'
 import Glitch from '../../assets/GlitchyText.svg'
 import TestBackground from '../../assets/Test-Portfolio-BackGround.svg'
 import CuteSkull from '../../assets/CuteSkull.png'
+import GooglePixelArt from '../../assets/GoogleBlockPixel.svg'
+import Bakery from '../../assets/bakery.svg'
 
 export default function testDesign() {
     return (
-        <Grid container spacing={2}>
-            <Grid container spacing={2} item lg={6} sm={12} xs={12}>
-                <Grid item lg={6} sm={12} xs={12}>
-                    <Item sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                        <img style={{ maxWidth: '90%', height: 'auto' }} src={HellDivers} alt="" />
-                    </Item>
+        // First Row
+        <>
+            <Grid container spacing={2}>
+                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxWidth: '90%', height: 'auto' }} src={HellDivers} alt="" />
+                        </Item>
+                    </Grid>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxWidth: '90%', height: 'auto' }} src={Glitch} alt="" />
+                        </Item>
+                    </Grid>
                 </Grid>
-                <Grid item lg={6} sm={12} xs={12}>
-                    <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                        <img style={{ maxWidth: '90%', height: 'auto' }} src={Glitch} alt="" />
-                    </Item>
+                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ height: '400px', maxWidth: '400px', borderRadius: '2%' }} src={TestBackground} alt="" />
+                        </Item>
+                    </Grid>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxWidth: '90%', height: 'auto', margin: '1px' }} src={CuteSkull} alt="" />
+                        </Item>
+                    </Grid>
                 </Grid>
+            </Grid >
+            {/* Second Row */}
+            <Grid container spacing={2}>
+                <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxWidth: '90%', height: 'auto' }} src={GooglePixelArt} alt="" />
+                        </Item>
+                    </Grid>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxHeight: '450px' }} src={Bakery} alt="A partial eaten donut logo that reads courtney's bakery, established in 2021" />
+                        </Item>
+                    </Grid>
+                </Grid>
+                {/* <Grid container spacing={2} item lg={6} sm={12} xs={12}>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ height: '400px', maxWidth: '400px', borderRadius: '2%' }} src={TestBackground} alt="" />
+                        </Item>
+                    </Grid>
+                    <Grid item lg={6} sm={12} xs={12}>
+                        <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
+                            <img style={{ maxWidth: '90%', height: 'auto', margin: '1px' }} src={CuteSkull} alt="" />
+                        </Item>
+                    </Grid>
+                </Grid> */}
             </Grid>
-            <Grid container spacing={2} item lg={6} sm={12} xs={12}>
-                <Grid item lg={6} sm={12} xs={12}>
-                    <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                        <img style={{ height: '400px', maxWidth: '400px', borderRadius: '2%' }} src={TestBackground} alt="" />
-                    </Item>
-                </Grid>
-                <Grid item lg={6} sm={12} xs={12}>
-                    <Item style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', background: 'rgba(250,250,250, 70%)', marginTop: '25px', height: "450px", border: "1px solid", borderRadius: '5px' }}>
-                        <img style={{ maxWidth: '90%', height: 'auto', margin: '1px' }} src={CuteSkull} alt="" />
-                    </Item>
-                </Grid>
-            </Grid>
-        </Grid >
+        </>
     )
 }
 
