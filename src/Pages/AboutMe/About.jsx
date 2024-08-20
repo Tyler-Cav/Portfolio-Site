508061320f24cf4df1635c4e970636f11131dd5d
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2024-08-20 19:45:42 -0400

    creating new aboutMe page layout with more detail. Working on more details about myself and using more cards to display a story like view. Figuring out stylized formatting

--- src/Pages/AboutMe/About.jsx
+++ src/Pages/AboutMe/About.jsx
@@ -4,6 +4,7 @@ import Box from "@mui/system/Box";
 import Grid from "@mui/material/Grid";
 import Item from "@mui/material/Grid"
 import DarkModeButton from './Components/darkmode'
+import { Padding } from '@mui/icons-material';
 
 function AboutMeWithDarkMode() {
   const [isActive, setIsActive] = useState(false);
@@ -17,6 +18,7 @@ function AboutMeWithDarkMode() {
         </Grid>
         <Grid item s={6} md={6} lg={6}>
           <Bio classNameChange='bioLightChange' />
+          <Bio classNameChange='bioLightChange' />
         </Grid>
       </Grid>
     )
@@ -28,10 +30,27 @@ function AboutMeWithDarkMode() {
             <DarkModeButton classNameChange='darkModeStyle' value={isActive} setActive={setIsActive} />
           </Item>
         </Grid>
-        <Grid item s={6} md={6} lg={6}>
+        <Grid item s={3} md={3} lg={3}>
           <Bio classNameChange='bioDarkChange' />
         </Grid>
-      </Grid>
+        <Grid sx={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center' }} item s={1} md={1} lg={1}>
+          <Box sx={{ color: 'white' }} >
+            psum dolor, sit amet consei magnam quas perspiciatis commodi?
+          </Box>
+        </Grid>
+        <Grid item s={3} md={3} lg={3}>
+          <Bio classNameChange='bioDarkChange' />
+        </Grid>
+        <Grid item s={1} md={1} lg={1}>
+          <h1>INSERT Arrow</h1>
+        </Grid>
+        <Grid item s={3} md={3} lg={3}>
+          <Bio classNameChange='bioDarkChange' />
+        </Grid>
+        <Grid item s={1} md={1} lg={1}>
+          <h1>INSERT Arrow</h1>
+        </Grid>
+      </Grid >
     )
   }
 }
