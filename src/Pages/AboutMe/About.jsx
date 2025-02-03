b33df0ea46598dd202d5deb7a13c6cc20e41c01f
Tyler Cavanaugh <Tyler.Cavanaugh@chargeitspot.com>
2025-02-03 17:26:04 -0500

    fixed merge conflicts

--- src/Pages/AboutMe/About.jsx
+++ src/Pages/AboutMe/About.jsx
@@ -4,16 +4,14 @@ import Grid from "@mui/system/Unstable_Grid";
 
 export default function About() {
   return (
-    <>
-      <Box component="section">
-        <Grid container spacing={0}>
-          <Grid s={12} md={1} lg={1}>
-          </Grid>
-          <Grid xs={12} lg={12}>
-            <Bio />
-          </Grid>
+    <Box component="section">
+      <Grid container spacing={0}>
+        <Grid s={12} md={1} lg={1}>
         </Grid>
-      </Box>
-    </>
+        <Grid xs={12} lg={12}>
+          <Bio />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
--- src/Pages/AboutMe/Components/bio.jsx
+++ src/Pages/AboutMe/Components/bio.jsx
@@ -1,19 +1,17 @@
 export default function Bio() {
   return (
-    <>
-      <section className="bio">
-        <h1>Bio</h1>
-        <p>A 2018 Business Management Graduate. </p>
-        <br></br>
-        <br></br>
-        <p>Recently finished a University of Pennsylvania fullstack web development program.</p>
-        <br></br>
-        <br></br>
-        <p>Excited to learn and add new projects to my portfolio.</p>
-        <br></br>
-        <br></br>
-        <p>Additional hobbies include vinyl collecting, building keyboards, cooking, and baking.</p>
-      </section>
-    </>
+    <section className="bio">
+      <h1>Bio</h1>
+      <p>A 2018 West Chester Business Management Graduate. </p>
+      <br></br>
+      <br></br>
+      <p>Recently finished a University of Pennsylvania fullstack web development program.</p>
+      <br></br>
+      <br></br>
+      <p>Excited to learn and add new projects to my portfolio.</p>
+      <br></br>
+      <br></br>
+      <p>Additional hobbies include vinyl collecting, building keyboards, cooking, and baking.</p>
+    </section>
   );
 }
