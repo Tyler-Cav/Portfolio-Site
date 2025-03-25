d092082a1d9e234effd4c6f112c939db6c2fa028
Tyler Cavanaugh <Tyler.Cavanaugh24@gmail.com>
2025-03-25 13:53:10 -0400

    updated Bio with formatting, description, and font

--- src/Pages/AboutMe/Components/bio.jsx
+++ src/Pages/AboutMe/Components/bio.jsx
@@ -2,17 +2,12 @@ export default function Bio() {
   return (
     <>
       <section className="bio">
-        <h1>Bio</h1>
-        <p>A 2018 Business Management Graduate. </p>
-        <br></br>
-        <br></br>
-        <p>Recently finished a University of Pennsylvania fullstack web development program.</p>
-        <br></br>
-        <br></br>
-        <p>Excited to learn and add new projects to my portfolio.</p>
-        <br></br>
-        <br></br>
-        <p>Additional hobbies include vinyl collecting, building keyboards, cooking, and baking.</p>
+        <h1>Who Am I?</h1>
+        <ul style={{display: "flex", flexDirection: "column", justifyContent: "space-between", height: "300px"}}>
+          <h3>Account Manager --&gt; Fullstack Web Developer </h3>
+          <h3>Recently completed a 6-month Software Engineer Internship at <a href="https://chargeitspot.com/">ChargeItSpot</a>.</h3>
+          <h3>Continuous learning is my goal & focus.</h3>
+        </ul>
       </section>
     </>
   );
