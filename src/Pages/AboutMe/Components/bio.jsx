24010166f9babc52e4eb3b818733fe3077b8d1a4
Tyler Cavanaugh <Tyler.Cavanaugh24@gmail.com>
2025-04-02 14:59:08 -0400

    added prettier package

--- src/Pages/AboutMe/Components/bio.jsx
+++ src/Pages/AboutMe/Components/bio.jsx
@@ -1,4 +1,4 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 Bio.propTypes = {
   classNameChange: PropTypes.string.isRequired,
@@ -12,12 +12,12 @@ Bio.propTypes = {
 export default function Bio(props) {
   return (
     <section className={props.classNameChange}>
-      <h1 style={{ fontSize: '25px' }}>{props.title}</h1>
+      <h1 style={{ fontSize: "25px" }}>{props.title}</h1>
       <p>{props.status}</p>
-      <p style={{ paddingTop: '5px' }} >{props.date}</p>
-      <ul style={{ paddingLeft: '15px' }}>
-        <li style={{ paddingTop: '15px' }}>{props.description}</li>
-        <li style={{ paddingTop: '15px' }}>{props.descriptionTwo}</li>
+      <p style={{ paddingTop: "5px" }}>{props.date}</p>
+      <ul style={{ paddingLeft: "15px" }}>
+        <li style={{ paddingTop: "15px" }}>{props.description}</li>
+        <li style={{ paddingTop: "15px" }}>{props.descriptionTwo}</li>
       </ul>
     </section>
   );
--- src/Pages/AboutMe/Components/darkmode.jsx
+++ src/Pages/AboutMe/Components/darkmode.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from 'react';
-import '../../../index.css'
+import React, { useState } from "react";
+import "../../../index.css";
 
-let buttonText = "Dark Mode"
+let buttonText = "Dark Mode";
 
 export default function Button(props) {
-    return (
-        <button
-            className={props.classNameChange}
-            onClick={() => {
-                if (props.value === false) {
-                    buttonText = "Light Mode"
-                    props.setActive(true)
-                } else {
-                    buttonText = "Dark Mode"
-                    props.setActive(false)
-                }
-            }
-            }>
-            {buttonText}
-        </button >
-    )
+  return (
+    <button
+      className={props.classNameChange}
+      onClick={() => {
+        if (props.value === false) {
+          buttonText = "Light Mode";
+          props.setActive(true);
+        } else {
+          buttonText = "Dark Mode";
+          props.setActive(false);
+        }
+      }}
+    >
+      {buttonText}
+    </button>
+  );
 }
-
