173050019d622bbdaf562ed943d12b35bbbe5c6d
Tyler Cavanaugh <Tyler.Cavanaugh24@gmail.com>
2025-04-02 18:45:50 -0400

    Projects now objects and mapped to function to better format to DRY.

--- src/Pages/Portfolio/components/Project-Cards.jsx
+++ src/Pages/Portfolio/components/Project-Cards.jsx
@@ -2,147 +2,86 @@ import avengersPic from "../../../assets/avengersAsset.png";
 
 let projects = [
   {
-    avengersLink: "https://rhemlock7.github.io/avenger-initiative/",
-    avengersPicture: avengersPic,
+    link: "https://rhemlock7.github.io/avenger-initiative/",
+    picture: avengersPic,
     alt: "Marvel Avengers Comic Book Photo including Captain America, Thor, Black Panther, and Iron Man",
     header: "Avengers Initiative",
     description:
       "A personal custom workout trainer built around Marvel character physique.",
   },
-  { weatherLink: "https://tyler-cav.github.io/WeatherTrackerAPI/ " },
-  { passGen: "https://tyler-cav.github.io/Password-Generator/" },
-  { SVG: "https://github.com/Tyler-Cav/SVG-Creator" },
-  { ReadMe: "https://github.com/Tyler-Cav/ReadMe-Template-Creator" },
-  { SQL: "https://github.com/Tyler-Cav/Back-End-eCommerce" },
-  { reaction: "https://reaction-simulator.netlify.app/" },
-  { scraper: "https://github.com/Tyler-Cav/JobScraper" },
+  {
+    link: "https://tyler-cav.github.io/WeatherTrackerAPI/ ",
+    picture: "",
+    alt: "",
+    header: "Weather Tracker",
+    description:
+      "A weather tracker in which you can input any city and recieve live weather information.",
+  },
+  {
+    link: "https://tyler-cav.github.io/Password-Generator/",
+    picture: "",
+    alt: "",
+    header: "Password Generator",
+    description: "A randomized password generator using custom alert prompts.",
+  },
+  {
+    link: "https://github.com/Tyler-Cav/SVG-Creator",
+    picture: "",
+    alt: "",
+    header: "SVG Shape Creator",
+    description:
+      "An SVG Logo Creator that uses CLI prompts with node to create SVG images.",
+  },
+  {
+    link: "https://github.com/Tyler-Cav/ReadMe-Template-Creator",
+    picture: "",
+    alt: "",
+    header: "ReadMe Creator",
+    description: "ReadMe template creator that can auto compile a readme.md.",
+  },
+  {
+    link: "https://github.com/Tyler-Cav/Back-End-eCommerce",
+    picture: "",
+    alt: "",
+    header: "SQL",
+    description:
+      "A backend created Ecommerce application using ExpressJS, MySQL, and Sequelize.",
+  },
+  {
+    link: "https://reaction-simulator.netlify.app/",
+    picture: "",
+    alt: "",
+    header: "REACTion",
+    description:
+      "A group created game site that tests someones reaction speed by clicking targets.",
+  },
+  {
+    link: "https://github.com/Tyler-Cav/JobScraper",
+    picture: "",
+    alt: "",
+    header: "Job Scraper",
+    description: "A python job scraper using a Listrak API.",
+  },
 ];
 
-const mapProjectCards = projects.map((project, index) => {
-  if (index === 0) {
-    return (
-      <>
-        <a id="aTag" href={project.avengersLink}>
-          <div className="cardFrame">
-            <img
-              id="portfolioImage"
-              src={project.avengersPicture}
-              alt={project.alt}
-            />
-            <div className="cardFrameTextBox">
-              <p id="cardMobileTextHeader">{project.header}</p>
-              <p id="cardMobileDescription">{project.description}</p>
-            </div>
+const mapProjectCards = projects.map((project, key) => {
+  return (
+    <>
+      <a id="aTag" href={project.link} key={project.key}>
+        <div className="cardFrame">
+          <img id="portfolioImage" src={project.picture} alt={project.alt} />
+          <div className="cardFrameTextBox">
+            <p id="cardMobileTextHeader">{project.header}</p>
+            <p id="cardMobileDescription">{project.description}</p>
           </div>
-        </a>
-      </>
-    );
-  }
+        </div>
+      </a>
+    </>
+  );
 });
 
 function ProjectCards() {
-  return (
-    <div className="projectBox">
-      <a id="aTag" href={projects[0].avengersLink}>
-        <div className="cardFrame">
-          <img
-            id="portfolioImage"
-            src={projects[0].avengersPicture}
-            alt="Marvel Avengers Comic Book Photo including Captain America, Thor, Black Panther, and Iron Man"
-          />
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">Avengers Initiative</p>
-            <p id="cardMobileDescription">
-              A personal custom workout trainer built around Marvel character
-              physique.
-            </p>
-          </div>
-        </div>
-      </a>
-      {mapProjectCards}
-      <a id="aTag" href={projects.weatherLink}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic} alt=""/> */}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">Weather Tracker</p>
-            <p id="cardMobileDescription">
-              A weather tracker in which you can input any city and recieve live
-              weather information.
-            </p>
-          </div>
-        </div>
-      </a>
-      <a id="aTag" href={projects.passGen}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic}*/}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">Password Generator</p>
-            <p id="cardMobileDescription">
-              A randomized password generator using custom alert prompts.
-            </p>
-          </div>
-        </div>
-      </a>
-      <a id="aTag" href={projects.SVG}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic}  */}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">SVG Shape Creator</p>
-            <p id="cardMobileDescription">
-              An SVG Logo Creator that uses CLI prompts with node to create SVG
-              images.
-            </p>
-          </div>
-        </div>
-      </a>
-      <a id="aTag" href={projects.ReadMe}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic}  */}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">ReadMe Creator</p>
-            <p id="cardMobileDescription">
-              ReadMe template creator that can auto compile a readme.md.
-            </p>
-          </div>
-        </div>
-      </a>
-      <a id="aTag" href={projects.SQL}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic} */}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">SQL</p>
-            <p id="cardMobileDescription">
-              A backend created Ecommerce application using ExpressJS, MySQL,
-              and Sequelize.
-            </p>
-          </div>
-        </div>
-      </a>
-      <a id="aTag" href={projects.reaction}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic} */}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">REACTion</p>
-            <p id="cardMobileDescription">
-              A group created game site that tests someones reaction speed by
-              clicking targets.
-            </p>
-          </div>
-        </div>
-      </a>
-      <a id="aTag" href={projects.scraper}>
-        <div className="cardFrame">
-          {/* <img id="portfolioImage" src={avengersPic} */}
-          <div className="cardFrameTextBox">
-            <p id="cardMobileTextHeader">Job Scraper</p>
-            <p id="cardMobileDescription">
-              A python job scraper using a Listrak API.
-            </p>
-          </div>
-        </div>
-      </a>
-    </div>
-  );
+  return <div className="projectBox">{mapProjectCards}</div>;
 }
 
 export default ProjectCards;
