5903b2a2eba64cb49770f1ba5bb4b0b87c7e348b
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-04-11 16:12:29 -0400

    fix to mobile

--- src/Pages/AboutMe/Components/bio.jsx
+++ src/Pages/AboutMe/Components/bio.jsx
@@ -8,9 +8,9 @@ export default function Bio() {
         <ul className="bio-list">
           <div>
             <p>From</p>
-            <h3><strong style={{ fontSize: "1.5em" }}>Account Manager</strong></h3>
+            <h3><strong style={{ fontWeight: "600" }}>Account Manager</strong></h3>
             <p>to</p>
-            <h3><strong style={{ fontSize: "3em" }}><span className='animate'>Fullstack Web Developer</span></strong></h3>
+            <h3><strong style={{ fontWeight: "600", fontSize: "1.2em", overFlowWrap: "break-word" }}>Fullstack Web Developer</strong></h3>
           </div>
           <div><h3 style={{ fontSize: "40px" }}>Recently completed a 6-month Software Engineer Internship at <a href="https://chargeitspot.com/">ChargeItSpot</a></h3></div>
           <h3 style={{ fontSize: "40px" }}>A passion for Frontend, UX/UI, and Data</h3>
