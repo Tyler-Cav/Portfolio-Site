0b9423af60ff017efe055634d13ce2f88d69f329
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2025-09-10 08:45:22 -0400

    Update about-me with new information

--- src/Pages/AboutMe/bio.jsx
+++ src/Pages/AboutMe/bio.jsx
@@ -11,7 +11,7 @@ export default function Bio() {
           <p>to</p>
           <h3><strong style={{ fontWeight: "600", fontSize: "1.2em", overFlowWrap: "break-word" }}>Designer & Developer</strong></h3>
         </div>
-        <div><h3 style={{ fontSize: "40px" }}>Recently completed a 6-month Software Engineer Internship at <a href="https://chargeitspot.com/">ChargeItSpot</a></h3></div>
+        <div><h3 style={{ fontSize: "40px" }}>Currently a UI Developer at <a href="https://www.onesky.xyz/">One Sky</a></h3></div>
         <h3 style={{ fontSize: "40px" }}>A passion for Frontend, Design, and Data</h3>
       </ul>
     </section >
